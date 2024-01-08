52f76f90746710c47558b4c9bda5c85529799a7f
harada1060 <118793247+harada1060@users.noreply.github.com>
2024-01-08 23:07:45 +0900

    Add files via upload

---
+++ Java-m_seikabutu/js/break.js
@@ -0,0 +1,26 @@
+function breakif() {
+	var num = document.getElementById('numberSelect').value;
+
+	var output = '';
+	for (var i = 0; i < 10; i++) {
+		output += i ;
+		if(i>=num){
+			break;
+		}
+	}
+	document.getElementById('output').innerHTML = output;
+}
+
+function continueif() {
+	var num = document.getElementById('numberSelect').value;
+
+	var output = '';
+	for (var i = 1; i < 20; i++) {
+		
+		if(i % num == 0 ){
+			continue;
+		}
+		output += i + "/";
+	}
+	document.getElementById('output').innerHTML = output;
+}
---
+++ Java-m_seikabutu/js/index2.js
@@ -0,0 +1,28 @@
+var fn = {};
+
+fn.load = function(page) {
+	var content = document.getElementById('myNavigator');
+	content.pushPage(page);
+};
+
+window.onload = function() {
+	var topics = {
+		'basic': '基礎',
+		'array': '配列',
+		'for': 'for文、while文',
+		'if': '条件分岐',
+		'break': 'break・continue',
+		'object': 'オブジェクト指向',
+		'exceptions': '例外処理'
+		// 他のカテゴリもここに追加
+	};
+
+	var categoryList = document.getElementById('category-list');
+	for (var topic in topics) {
+		var listItem = document.createElement('ons-list-item');
+		listItem.setAttribute('tappable', '');
+		listItem.setAttribute('onclick', "fn.load('" + topic + ".html')");
+		listItem.textContent = topics[topic];
+		categoryList.appendChild(listItem);
+	}
+};
